c062623d592eb1cf05f76ecec4d5229949d55ac3
Raheel <rahweelshehzad188@gmail.com>
2023-06-25 22:33:53 +0500

    dashcode home2 page done

--- application/libraries/Template.php
+++ application/libraries/Template.php
@@ -355,7 +355,7 @@ class Template {
 
                 
 // 
-                // echo $fot =  $CI->load->view(config_item('app_theme').'/includes/footer',$data,true);
+                echo $fot =  $CI->load->view(config_item('app_theme').'/includes/footer',$data,true);
 
                 
 
--- application/views/dashcode/includes/footer.php
+++ application/views/dashcode/includes/footer.php
@@ -1,229 +1,68 @@
-            </div>
-        </div>
-    </div>
-<!-- BEGIN: Footer-->
-    <footer class="footer footer-static footer-light">
-        <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2020<a class="text-bold-800 grey darken-2" href="http://Channelsmedia.com/" target="_blank">Channelsmedia.com,</a>All rights Reserved</span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i class="feather icon-heart pink"></i></span>
-            <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="feather icon-arrow-up"></i></button>
-        </p>
-    </footer>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <script type="text/javascript">
-        var BASE_URL = "<?=(isset($url)?$url:base_url())?>";
-    </script>
-    <!-- END: Footer-->
-    <!-- START: Send Sms-->
-    <!-- Modal -->
-  <div class="modal fade" id="sndSmsModal" role="dialog">
-    <div class="modal-dialog">
-    <div class="forloader"><div class="loader"></div></div>
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title"></h4>
-        </div>
-        <div class="modal-body">
-          <div class="form">
-              <input type="text" name=" number" id="cnum" class="form-control"/>
-              <input type="texxt" name="couponID" id = "couponID"/ style="visibility:hidden; ">
-              <input type="texxt" name="type" id = "rtype" style="visibility:hidden; ">
-              <button id="modsubmit" onclick="send_single()" class="vs-button-text vs-button--text">Send</button>
-          </div>
-        </div>
-        
-      
-    </div>
-  </div>
-    <!-- End: Send Sms-->
-
-
-    <!-- BEGIN: Vendor JS-->
-    <script src="<?= $assets?>app-assets/vendors/js/vendors.min.js"></script>
-    <!-- BEGIN Vendor JS-->
-
-    <!-- BEGIN: Page Vendor JS-->
-    <script src="<?= $assets?>app-assets/vendors/js/charts/apexcharts.min.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/extensions/tether.min.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/extensions/shepherd.min.js"></script>
-    <!-- END: Page Vendor JS-->
-
-    <!-- BEGIN: Theme JS-->
-    <script src="<?= $assets?>app-assets/js/core/app-menu.js"></script>
-    <script src="<?= $assets?>app-assets/js/core/app.js"></script>
-    <script src="<?= $assets?>app-assets/js/scripts/components.js"></script>
-    <!-- END: Theme JS-->
-    <!---all tags--->
-    <?php
-    if(isset($js))
-    {
-        foreach ($js as $key => $value) {
-            ?>
-           
-            <script src="<?= $value; ?>"></script
-            <?php
-        }    }
-    ?>
-        <!-- BEGIN: Page Vendor JS-->
-    <script src="<?= $assets?>app-assets/vendors/js/tables/datatable/pdfmake.min.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/tables/datatable/vfs_fonts.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/tables/datatable/datatables.min.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/tables/datatable/datatables.buttons.min.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/tables/datatable/buttons.html5.min.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/tables/datatable/buttons.print.min.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js"></script>
-    <script src="<?= $assets?>app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js"></script>
-
-    <!-- BEGIN: Page JS-->
-    <script src="<?= $assets?>app-assets/js/scripts/datatables/datatable.js"></script>
-    <!-- END: Page JS-->
-    <script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
-
-
-    <!-- BEGIN: Page JS-->
-    <script src="<?= $assets?>app-assets/js/scripts/pages/dashboard-analytics.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-    <!-- END: Page JS-->
-    <script type="text/javascript">
-        $(document).ready(function() {
-    $('.select2').select2();
-});
-        function sendSingle(id)
-        {
-            $('#modsubmit').text('Assign');
-            $('#sndSmsModal').modal('show');
-            $('#couponID').val(id);
-
-        }
-        function send_single(){
-            var type=$('#rtype').val();
-            var data = {
-    number: $('#cnum').val(),
-    coupon: $('#couponID').val(),
-    type: $('#rtype').val()
-  };
-            $.post("<?= base_url();?>/api/send_coupon",
-  data,
-  function(data, status){
-    var obj = JSON.parse(data);
-    console.log(obj);
-    if(obj.status)
-    {
-        swal("Good job!", obj.msg, "success");
-        location.reload();
-
-    }
-    else
-    {
-        swal({
-  title: "Sorry!",
-  text: obj.msg,
-  icon: "error",
-});
-}
-  });
-        }
-        function send_group()
-        {
-            var cop = '';
-            $('.coupon_check').each(function(i, obj) {
-                console.log(i);
-                if($(this).prop("checked"))
-                {
-                    cop += $(this).val()+',';
-                    // console.log($(this).val());
-                }
-            });
-     $('#sndSmsModal').modal('show');
-            $('#couponID').val(cop);
-        }
-        function copyToClipboard(id) {
-            // var text = $(this).attr('link');
-            // console.log(text);
-            var link = $("#client"+id).val()+'/'+id;
-            alert("Copy Link:"+link);
-             
-    
-  
-}
-function send_link(type, user)
-{
-    $('#sndSmsModal').modal('show');
-            if(type == 'sms')
-            {
-                var mid = '#phone'+user;
-            $('#cnum').attr('placeholder','Enter Phone Number');
-            $('#cnum').val($(mid).text());
-            }
-            else
-            {
-                
-                $('#cnum').attr('placeholder','Enter Email Address');
-            }
-            $('#rtype').val(type);
-            $('#couponID').val(user);
-}
-
-function vid_course()
-{
-    $('#vid_mod').html('<option value="0">Loading</option');
-    $.get("<?= base_url();?>/api/vid_course/"+$('#vidless').val(),{},
-  function(data, status){
-    if(data)
-    {
-    $('#vid_mod').html(data);
-    }
-    else
-    {
-        alert("No module exist");
-    }
-});
-}
-function use_coupon(id)
-{
-    var data = {
-    number: 'null',
-    coupon: id,
-    type: 'use'
-  };
-    $.post("<?= base_url();?>/api/send_coupon",
-  data,
-  function(data, status){
-    var obj = JSON.parse(data);
-    console.log(obj);
-    if(obj.status)
-    {
-        swal("Good job!", obj.msg, "success");
-        location.reload();
-
-    }
-    else
-    {
-        swal({
-  title: "Sorry!",
-  text: obj.msg,
-  icon: "error",
-});
-}
-});
-}
-    </script>
-    <script type="text/javascript">
-                    //after_lesson
-                    $("#after_lesson").change(function() {
-    if(this.checked) {
-        $('#lesson_div').show();
-    }
-    else
-    {
-        $('#lesson_div').hide();
-    }
-});
-                </script>
-
-</body>
-<!-- END: Body-->
-
+
+      <!-- BEGIN: Footer For Desktop and tab -->
+      <footer class="md:block hidden" id="footer">
+        <div class="site-footer px-6 bg-white dark:bg-slate-800 text-slate-500 dark:text-slate-300 py-4 ltr:ml-[248px] rtl:mr-[248px]">
+          <div class="grid md:grid-cols-2 grid-cols-1 md:gap-5">
+            <div class="text-center ltr:md:text-start rtl:md:text-right text-sm">
+              COPYRIGHT ©
+              <span id="thisYear"></span>
+              DashCode, All rights Reserved
+            </div>
+            <div class="ltr:md:text-right rtl:md:text-end text-center text-sm">
+              Hand-crafted &amp; Made by
+              <a href="https://codeshaper.net" target="_blank" class="text-primary-500 font-semibold">
+                Codeshaper
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!-- END: Footer For Desktop and tab -->
+
+      <div class="bg-white bg-no-repeat custom-dropshadow footer-bg dark:bg-slate-700 flex justify-around items-center
+    backdrop-filter backdrop-blur-[40px] fixed left-0 bottom-0 w-full z-[9999] bothrefm-0 py-[12px] px-4 md:hidden">
+        <a href="chat.html">
+          <div>
+            <span class="relative cursor-pointer rounded-full text-[20px] flex flex-col items-center justify-center mb-1 dark:text-white
+          text-slate-900 ">
+        <iconify-icon icon="heroicons-outline:mail"></iconify-icon>
+        <span class="absolute right-[5px] lg:hrefp-0 -hrefp-2 h-4 w-4 bg-red-500 text-[8px] font-semibold flex flex-col items-center
+            justify-center rounded-full text-white z-[99]">
+          10
+        </span>
+            </span>
+            <span class="block text-[11px] text-slate-600 dark:text-slate-300">
+        Messages
+      </span>
+          </div>
+        </a>
+        <a href="profile.html" class="relative bg-white bg-no-repeat backdrop-filter backdrop-blur-[40px] rounded-full footer-bg dark:bg-slate-700
+      h-[65px] w-[65px] z-[-1] -mt-[40px] flex justify-center items-center">
+          <div class="h-[50px] w-[50px] rounded-full relative left-[0px] hrefp-[0px] custom-dropshadow">
+            <img src="<?= $assets; ?>assets/images/users/user-1.jpg" alt="" class="w-full h-full rounded-full border-2 border-slate-100">
+          </div>
+        </a>
+        <a href="#">
+          <div>
+            <span class=" relative cursor-pointer rounded-full text-[20px] flex flex-col items-center justify-center mb-1 dark:text-white
+          text-slate-900">
+        <iconify-icon icon="heroicons-outline:bell"></iconify-icon>
+        <span class="absolute right-[17px] lg:hrefp-0 -hrefp-2 h-4 w-4 bg-red-500 text-[8px] font-semibold flex flex-col items-center
+            justify-center rounded-full text-white z-[99]">
+          2
+        </span>
+            </span>
+            <span class=" block text-[11px] text-slate-600 dark:text-slate-300">
+        Notifications
+      </span>
+          </div>
+        </a>
+      </div>
+    </div>
+  </main>
+  <!-- scripts -->
+  <script src="<?= $assets; ?>assets/js/jquery-3.6.0.min.js"></script>
+  <script src="<?= $assets; ?>assets/js/rt-plugins.js"></script>
+  <script src="<?= $assets; ?>assets/js/app.js"></script>
+</body>
 </html>
